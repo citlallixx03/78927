2a513524b0cf54fbf955b02945f243320fffc0c0
citlallixx03 <2282785651citlali@gmail.com>
2024-04-06 10:33:20 -0600

    practica03

---
+++ practica03/gui.java
@@ -0,0 +1,10 @@
+public class gui {
+    
+    public static void main(String[] args) {
+        calculadora c= new calculadora();
+        System.out.println(c.suma(1,2));
+        System.out.println(c.resta(1,2));
+        System.out.println(c.division(1,2));
+        System.out.println(c.multiplicacion(1,2));
+    }
+}
